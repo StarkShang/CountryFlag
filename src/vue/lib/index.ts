a1e89f64288672d007b47943f54ae1f02a072e88
StarkShang <stark.shang@sjtu.edu.cn>
2021-01-31 02:49:48 +0800

    fix: 修复没注册组件的bug

--- src/vue/lib/index.ts
+++ src/vue/lib/index.ts
@@ -1,12 +1,11 @@
-import Vue from "vue";
+import Vue, { VueConstructor } from "vue";
 import CountryFlag from "./components/CountryFlag.vue";
 const req = require.context("circle-flags/flags", true, /\.svg$/);
 req.keys().map(req);
 
-function install() {
-    Vue.component(CountryFlag.name, CountryFlag);
+function install(vue: VueConstructor<Vue>) {
+    vue.component(CountryFlag.name, CountryFlag);
 }
 
-export default {
-    install
-}
+export * from "./models";
+export default install;
